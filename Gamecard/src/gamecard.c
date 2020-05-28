f7806c29a2afbd08e3aaeb5f7f3eccc2285d18c4
santisanchez0 <santisanchez1999@gmail.com>
2020-05-27 23:45:44 -0300

    agrego base para Gamecard

---
+++ Gamecard/src/gamecard.c
@@ -0,0 +1,6 @@
+#include "gamecard.h"
+
+int main(void) {
+	//inicializar_game_card();
+	return EXIT_SUCCESS;
+}
---
+++ Gamecard/src/gamecard.h
@@ -0,0 +1,9 @@
+#ifndef GAMECARD_H
+#define GAMECARD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+
+
+#endif /* GAMECARD_H*/
