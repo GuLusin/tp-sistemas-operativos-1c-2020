e3d9a2c3d2751e92f6e5ef82e64a9b4fda938f9b
Nahuelgp <nahuelgp2000@hotmail.com>
2020-05-29 01:40:11 -0300

    Agregamos codigo del gamecard

---
+++ Gamecard/src/gamecard.h
@@ -0,0 +1,21 @@
+#ifndef GAMECARD_H
+#define GAMECARD_H
+
+#include <utils/conexiones.h>
+#include <utils/mensajes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/log.h>
+#include <commons/string.h>
+#include <commons/config.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <commons/collections/list.h>
+
+t_config* config;
+t_log* logger;
+int tiempo_reintento_conexion;
+int tiempo_reintento_operacion;
+char *ip_broker, *puerto_broker, *punto_montaje;
+
+#endif /* GAMECARD_H*/
