5c2ab6820ce6d19f033eb475930e925b11bd5097
Madness <49067443+NicoMadd@users.noreply.github.com>
2020-07-18 18:37:50 -0300

    Add files via upload

---
+++ Gamecard/Debug/Debug/src/gamecard.d
@@ -0,0 +1,15 @@
+src/gamecard.o: ../src/gamecard.c ../src/gamecard.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h
+
+../src/gamecard.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h:
