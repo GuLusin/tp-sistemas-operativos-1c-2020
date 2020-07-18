78ccac5c1dc9a5c84d230050f853a4ab6186f8de
Madness <49067443+NicoMadd@users.noreply.github.com>
2020-07-18 18:38:29 -0300

    Add files via upload

---
+++ Team/Debug/src/team.d
@@ -0,0 +1,15 @@
+src/team.o: ../src/team.c ../src/team.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h \
+ /home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h
+
+../src/team.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/conexiones.h:
+
+/home/utnso/workspace/tp-2020-1c-cumpleDeFabian/utils/utils/mensajes.h:
