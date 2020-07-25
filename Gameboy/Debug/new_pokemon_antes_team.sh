941ad47e3e8d0c2f835cba9e6804c41a92c8046b
Madness <49067443+NicoMadd@users.noreply.github.com>
2020-07-25 14:29:39 -0300

    Add files via upload

---
+++ Gameboy/Debug/new_pokemon_antes_team.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+./gameboy BROKER NEW_POKEMON Pikachu 9 9 1
+./gameboy BROKER NEW_POKEMON Squirtle 5 2 1
+
+./gameboy BROKER NEW_POKEMON Jolteon 2 2 1
+./gameboy BROKER NEW_POKEMON Flareon 4 6 1
---
+++ Gameboy/Debug/new_pokemon_post_team.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./gameboy BROKER NEW_POKEMON Onix 2 8 1
+./gameboy BROKER NEW_POKEMON Gengar 6 6 1
+./gameboy BROKER NEW_POKEMON Squirtle 5 5 1
+
+./gameboy BROKER NEW_POKEMON Umbreon 10 6 1
+./gameboy BROKER NEW_POKEMON Espeon 7 1 1
+./gameboy BROKER NEW_POKEMON Vaporeon 4 10 1
